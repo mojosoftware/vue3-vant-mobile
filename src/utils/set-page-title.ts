ce18ab9c55a30d68c3b303f5b871612de9fe760f
mojo <811530622@qq.com>
2025-08-17 01:35:11 +0800

    Updated set-page-title.ts

--- src/utils/set-page-title.ts
+++ src/utils/set-page-title.ts
@@ -1,5 +1,5 @@
 import { appName } from '@/constants'
 
 export default function setPageTitle(name?: string): void {
-  window.document.title = name ? `${i18n.global.t(`navbar.${name}`)} - ${appName()}` : appName()
+  window.document.title = name ? `${appName()}` : appName()
 }
